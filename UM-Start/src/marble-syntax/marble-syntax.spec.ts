1204b81d831bde8f8d61bd67a049d31756dcf7a5
Michael Kariuki <michael.kariuki@siemens-healthineers.com>
2022-03-03 22:14:19 +0100

    practice(UM-Start/marble-syntax): Added test for empty observables

--- UM-Start/src/marble-syntax/marble-syntax.spec.ts
+++ UM-Start/src/marble-syntax/marble-syntax.spec.ts
@@ -1,18 +1,28 @@
 import { cold } from 'jasmine-marbles';
 import { NEVER, EMPTY } from 'rxjs';
 
+/**
+ * EMPTY : observables with no items but terminates normally represented by (|)
+ * NEVER : emits no items and does not terminate, represented by (-) or (----)
+
+*/
+
 describe('Marble Syntax', () => {
   describe('EMPTY', () => {
     it('emits no items but terminates normally', () => {
+      const inputs = EMPTY;
+      const results = cold('|');
 
-
+      expect(inputs).toBeObservable(results);
     });
   });
 
   describe('NEVER', () => {
     it('emits no items and does not terminate', () => {
-     
+      const input = NEVER;
+      const results = cold('-');
 
+      expect(input).toBeObservable(results);
     });
   });
 });

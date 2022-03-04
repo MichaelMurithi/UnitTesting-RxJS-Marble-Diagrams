59657b0d963f478e682eec568dd875aa9f2e5a02
Michael Kariuki <michael.kariuki@siemens-healthineers.com>
2022-03-04 11:12:06 +0100

    practice(UM-Start): Practiced testing hot and cold observables with mock values

--- UM-Start/src/hot-cold/cold.spec.ts
+++ UM-Start/src/hot-cold/cold.spec.ts
@@ -1,19 +1,35 @@
-import { cold, getTestScheduler } from 'jasmine-marbles';
-import { of, Observable, throwError, from } from 'rxjs';
+import { cold } from 'jasmine-marbles';
+import { from, EMPTY } from 'rxjs';
 
 describe('COLD', () => {
-  it('of with one value', () => {
-
+  it('can search for a name', () => {
+    const input = search('e');
+    const expected = cold('(e|)');
+    expect(input).toBeObservable(expected);
   });
 
-  it('of with 2 values', () => {
+  it('can return empty when no value found', () => {
+    const input = search('B');
+    const expected = cold('|');
 
+    expect(input).toBeObservable(expected);
   });
 
-  it('from', () => {
+  it('can return all vowels from alphabet list', () => {
+    const input = search('', 'v');
+    const expected = cold('(aeiou|)');
 
-  });
-
-  it('should trim the spaces', () => {
+    expect(input).toBeObservable(expected);
   });
 });
+
+const letters = ['a', 'e', 'i', 'o', 'u', 'b', 'c', 'd'];
+
+
+function search(key, refference = 'l') {
+  if (refference === 'v') {
+    const vowels = letters.filter(letter => ['a', 'e', 'i', 'o', 'u'].indexOf(letter) > -1 ? true : false);
+    return from(vowels);
+  }
+  return letters.indexOf(key) > -1 ? from(`${key}`) : EMPTY;
+}
--- UM-Start/src/hot-cold/hot.spec.ts
+++ UM-Start/src/hot-cold/hot.spec.ts
@@ -1,14 +1,34 @@
 import { hot, cold } from 'jasmine-marbles';
-import { from } from 'rxjs';
-import { share } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 
 describe('hot', () => {
 
-  it('should test subscription', () => {
-
+  it('should test subscription on hot observable', () => {
+    const input = hot('-a-^b---c-|');
+    const subscription = '^------!';
+    expect(input).toBeObservable(cold('-b---c-|'));
+    expect(input).toHaveSubscriptions(subscription);
   });
 
-  it('should test subscription observable', () => {
+  it('should test subscription on hot observables that never completes', () => {
+    const provided = hot('-a-^(bc)--');
+    const subscription = '^--';
 
+    expect(provided).toBeObservable(cold('-(bc)--'));
+    expect(provided).toHaveSubscriptions(subscription);
   });
+
+  it('can convert names to uppercase', () => {
+    const names = hot('--vladmir--putin--zelensky--joe--biden--');
+    const inputs = convertToUpperCase(names);
+    const subscription = '^--';
+
+    expect(inputs).toBeObservable(cold('--VLADMIR--PUTIN--ZELENSKY--JOE--BIDEN--'));
+    expect(names).toHaveSubscriptions(subscription);
+  })
 });
+
+function convertToUpperCase($alphabets) {
+  return $alphabets.pipe(switchMap((s: string) => s.toUpperCase()));
+}
+
